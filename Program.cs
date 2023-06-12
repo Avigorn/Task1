cc337065db98de5eac9aa03e0452dfa4cea906ea
Alex <ulkiorra@gmail.com>
2023-06-12 19:40:22 +0300

    Четвёртая задача

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
-﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
 
-Console.Clear(); // Очистка экрана
+Console.Clear();
 Console.Write("Введите число: ");
 string num = Console.ReadLine();
 int value = Convert.ToInt32(num);
+for (int i = 0; i <= value; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + " ");
+    }
+}
 
-if (value % 2 == 0)
-{
-    System.Console.WriteLine("Число чётное");
-}
-else
-{
-    System.Console.WriteLine("Число нечётное");
-}
+System.Console.WriteLine();
