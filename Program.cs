e8c410ca52d7be951d7cfae9054cb6dacb4d3e76
Alex <ulkiorra@gmail.com>
2023-06-12 17:18:49 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 Console.Clear(); // Очистка экрана
 Console.Write("Введите первое число: ");
 string num1 = Console.ReadLine();
 Console.Write("Введите второе число: ");
 string num2 = Console.ReadLine();
+Console.Write("Введите третье число: ");
+string num3 = Console.ReadLine();
 int number1 = Convert.ToInt32(num1);
 int number2 = Convert.ToInt32(num2);
-int max = 0;
+int number3 = Convert.ToInt32(num3);
+int maximum = Math.Max(number1, Math.Max(number2, number3));
 
-if (number1 > number2)
-{
-    max = number1;
-}
-else max = number2;
-
-Console.WriteLine(max);
+System.Console.WriteLine(maximum);
